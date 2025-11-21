b7f42d255e83ac9fa24f545ee42da5d867f90849
Sigma <168486811+sigma7863@users.noreply.github.com>
2025-11-21 10:27:05 +0900

    Create 03-create-kotlin-project.kt

---
+++ 03-create-kotlin-project.kt
@@ -0,0 +1,18 @@
+package com.example.kotlinproject
+
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+
+// アプリケーションの入り口
+fun main() {
+    println("Hello, world!")
+}
+/*
+  main関数は
+  アプリケーションの入り口です
+*/
+fun main() {
+    println("Hello, world!")
+}
